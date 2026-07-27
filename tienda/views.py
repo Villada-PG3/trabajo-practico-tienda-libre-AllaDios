130286c275424431d0ab7aff94e1a9ce4e13c238
AllaDios <lautaroalladio12@gmail.com>
2026-07-27 11:59:55 +0000

    primera entrega

--- tienda/views.py
+++ tienda/views.py
@@ -1,16 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Producto
 
 def lista_productos(request):
     productos = Producto.objects.all()
 
     return render(
-                request,
-        "productos.html"
+        request,
+        "productos.html",
         {
             "productos": productos
         }
     )
+
 
 def detalle_producto(request, id):
     producto = get_object_or_404(
