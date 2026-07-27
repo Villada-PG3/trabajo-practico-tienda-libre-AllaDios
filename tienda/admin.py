e8ef61273eb6aee6fa5b766ca932d5989eaec4d0
AllaDios <lautaroalladio12@gmail.com>
2026-07-27 11:47:33 +0000

    primera entrega

---
+++ tienda/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+from tienda.models import Producto
+
+admin.site.register(Producto)
---
+++ tienda/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from .models import Producto
+
+def lista_productos(request):
+    productos = Producto.objects.all()
+
+    return render(
+                request,
+        "productos.html"
+        {
+            "productos": productos
+        }
+    )
+
+def detalle_producto(request, id):
+    producto = get_object_or_404(
+        Producto,
+        id=id
+    )
+
+    return render(
+        request,
+        "detalle_producto.html",
+        {
+            "producto": producto
+        }
+    )
