cd3298d1d27c488ddd0be0e656b70e20c905db48
AllaDios <lautaroalladio12@gmail.com>
2026-08-03 15:01:38 +0000

    feat: primera vista y template para home y acerca-de-mi.

---
+++ tienda/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = 'tienda'
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("acerca-de-mi/", views.acerca_de_mi, name="acerca_de_mi")
+]
--- tienda/views.py
+++ tienda/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 from .models import Producto
 
+def home(request):
+    return render(request, "tienda/home.html")
+
+def acerca_de_mi(request):
+    return render(request, 'tienda/acerca-de-mi.html')
